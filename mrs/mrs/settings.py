d736aa6d5836ccbc3e4fb4233d0828b0bcbcebcc
Charan <charankulal0241@gmail.com>
2024-03-31 08:55:51 +0530

    Created abstract user model

--- mrs/mrs/settings.py
+++ mrs/mrs/settings.py
@@ -31,14 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base'
+    'base.apps.BaseConfig',
+    'corsheaders',
+    
 ]
+
+AUTH_USER_MODEL='base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
